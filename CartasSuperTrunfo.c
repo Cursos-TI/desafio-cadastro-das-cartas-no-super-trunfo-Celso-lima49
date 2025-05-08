2e88aa7018260d068ce24c1f42335fde7dce763b
Celso-lima49 <megacell10@yahoo.com.br>
2025-05-08 22:19:47 +0000

    Densidade e per capita

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,19 +5,16 @@ int main() {
     char estado1;
     char codigo1[4];   
     char nomeCidade1[50];
-    int populacao1;
-    float area1;
-    float pib1;
-    int pontosTuristicos1;
-
+    int populacao1, pontosTuristicos1;
+    float area1, pib1, densidade_pop1, pib_per_capita1;
+     
     // Declaração de variáveis da Carta 2
     char estado2;
     char codigo2[4];
     char nomeCidade2[50];
-    int populacao2;
-    float area2;
-    float pib2;
-    int pontosTuristicos2;
+    int populacao2, pontosTuristicos2;
+    float area2, pib2, densidade_pop2, pib_per_capita2;
+      
 
     // Entrada de dados da Carta 1
     printf("Cadastro da Carta 1:\n");
@@ -35,6 +32,13 @@ int main() {
     scanf(" %f", &pib1);
     printf("Digite o número de pontos turísticos: ");
     scanf(" %d", &pontosTuristicos1);
+    
+
+    // Cálculo da Densidade e PIB per capita para a Carta 1
+    densidade_pop1 =  populacao1 / area1;
+    pib_per_capita1 =  (pib1 * 1000000000) / populacao1;
+    
+    
 
     printf("\n"); // pular linha dos dados da carta 01 para carta 02.
 
@@ -43,17 +47,21 @@ int main() {
     printf("Digite a letra do estado (A a H): ");
     scanf(" %c", &estado2);
     printf("Digite o código da carta (ex: B02): ");
-    scanf("%s", codigo2);
+    scanf(" %s", codigo2);
     printf("Digite o nome da cidade: ");
     scanf(" %[^\n]", nomeCidade2); // %[^\n], possibilita escrever nome de cidades usando o espaço ex: rio dos bois.
     printf("Digite o número de habitantes: ");
-    scanf("%d", &populacao2);
+    scanf(" %d", &populacao2);
     printf("Digite a área (em km²): ");
     scanf("%f", &area2);
     printf("Digite o PIB (em bilhões): ");
-    scanf("%f", &pib2);
+    scanf(" %f", &pib2);
     printf("Digite o número de pontos turísticos: ");
-    scanf("%d", &pontosTuristicos2);
+    scanf(" %d", &pontosTuristicos2);
+
+    // Cálculo da Densidade e PIB per capita para a Carta 2
+    densidade_pop2 =  populacao2 / area2;
+    pib_per_capita2 =  (pib2 * 1000000000) / populacao2;
 
     printf("\n");
 
@@ -66,6 +74,9 @@ int main() {
     printf("Área: %.2f km²\n", area1);
     printf("PIB: %.2f bilhões de reais\n", pib1);
     printf("Número de Pontos Turísticos: %d\n", pontosTuristicos1);
+    printf("Densidade Populacional é: %.2f\n", densidade_pop1);
+    printf("PIB per Capita é: %.2f\n", pib_per_capita1);
+   
 
     printf("\n");
 
@@ -78,6 +89,8 @@ int main() {
     printf("Área: %.2f km²\n", area2);
     printf("PIB: %.2f bilhões de reais\n", pib2);
     printf("Número de Pontos Turísticos: %d\n", pontosTuristicos2);
+    printf("Densidade Populacional é: %.2f\n", densidade_pop2);
+    printf("PIB per Capita é: %.2f\n", pib_per_capita2);
 
     return 0;
 }
